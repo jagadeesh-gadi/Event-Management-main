b8ac28f57720c8542397ff7feae76c54d225bc64
jagadeesh <95517099+jagadeesh-gadi@users.noreply.github.com>
2025-12-28 12:24:01 +0530

    Update Hotel.java

--- src/main/java/com/app/demo/model/Hotel.java
+++ src/main/java/com/app/demo/model/Hotel.java
@@ -1,3 +1,102 @@
+// package com.app.demo.model;
+
+// import java.util.List;
+
+// import javax.persistence.CascadeType;
+// import javax.persistence.Column;
+// import javax.persistence.Entity;
+// import javax.persistence.FetchType;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+// import javax.persistence.OneToMany;
+// import javax.persistence.Table;
+
+
+// @Entity
+// @Table(name = "hotel")
+
+// public class Hotel {
+
+//    @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+// 	private int id;
+    
+//     @Column(name="hotel_name")
+// 	private String hotelName;
+    
+//     @Column(name="hotel_desc")
+// 	private String hotelDesc;
+    
+//     @Column(name="hotel_img1",columnDefinition = "longblob")
+// 	private String hotelImg1;
+
+//     @Column(name="price")
+//     private int price;
+    
+//     @Column(name="location")
+//     private String location;
+
+//     @OneToMany(mappedBy="hotel",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+// 	private List<Booking> booking;
+    
+// 	public int getId() {
+// 		return this.id;
+// 	}
+
+// 	public void setId(int id) {
+// 		this.id = id;
+// 	}
+
+// 	public String getHotelName() {
+// 		return hotelName;
+// 	}
+
+// 	public void setHotelName(String hotelName) {
+// 		this.hotelName = hotelName;
+// 	}
+
+// 	public String getHotelDesc() {
+// 		return hotelDesc;
+// 	}
+
+// 	public void setHotelDesc(String hotelDesc) {
+// 		this.hotelDesc = hotelDesc;
+// 	}
+
+// 	public String getHotelImg1() {
+// 		return hotelImg1;
+// 	}
+
+// 	public void setHotelImg1(String hotelImg1) {
+// 		this.hotelImg1 = hotelImg1;
+// 	}
+
+// 	public int getPrice() {
+// 		return price;
+// 	}
+
+// 	public void setPrice(int price) {
+// 		this.price = price;
+// 	}
+
+// 	public String getLocation() {
+// 		return location;
+// 	}
+
+// 	public void setLocation(String location) {
+// 		this.location = location;
+// 	}
+
+// 	@Override
+// 	public String toString() {
+// 		return "Hotel [Id=" + id + ", hotelName=" + hotelName + ", hotelDesc=" + hotelDesc + ", hotelImg1=" + hotelImg1
+// 				+ ", price=" + price + ", location=" + location + "]";
+// 	}
+
+	
+// }
+
 package com.app.demo.model;
 
 import java.util.List;
@@ -9,90 +108,107 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "hotel")
-
 public class Hotel {
 
-   @Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-    
-    @Column(name="hotel_name")
-	private String hotelName;
-    
-    @Column(name="hotel_desc")
-	private String hotelDesc;
-    
-    @Column(name="hotel_img1",columnDefinition = "longblob")
-	private String hotelImg1;
+    private int id;
 
-    @Column(name="price")
+    @Column(name = "hotel_name")
+    private String hotelName;
+
+    @Column(name = "hotel_desc")
+    private String hotelDesc;
+
+    // ✅ FIXED: longblob should NOT be String
+    @Lob
+    @Column(name = "hotel_img1")
+    private byte[] hotelImg1;
+
+    @Column(name = "price")
     private int price;
-    
-    @Column(name="location")
+
+    @Column(name = "location")
     private String location;
 
-    @OneToMany(mappedBy="hotel",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
-	private List<Booking> booking;
-    
-	public int getId() {
-		return this.id;
-	}
+    // ✅ Prevent infinite JSON loop
+    @JsonIgnore
+    @OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Booking> booking;
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    // ===== GETTERS & SETTERS =====
 
-	public String getHotelName() {
-		return hotelName;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setHotelName(String hotelName) {
-		this.hotelName = hotelName;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getHotelDesc() {
-		return hotelDesc;
-	}
+    public String getHotelName() {
+        return hotelName;
+    }
 
-	public void setHotelDesc(String hotelDesc) {
-		this.hotelDesc = hotelDesc;
-	}
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
 
-	public String getHotelImg1() {
-		return hotelImg1;
-	}
+    public String getHotelDesc() {
+        return hotelDesc;
+    }
 
-	public void setHotelImg1(String hotelImg1) {
-		this.hotelImg1 = hotelImg1;
-	}
+    public void setHotelDesc(String hotelDesc) {
+        this.hotelDesc = hotelDesc;
+    }
 
-	public int getPrice() {
-		return price;
-	}
+    public byte[] getHotelImg1() {
+        return hotelImg1;
+    }
 
-	public void setPrice(int price) {
-		this.price = price;
-	}
+    public void setHotelImg1(byte[] hotelImg1) {
+        this.hotelImg1 = hotelImg1;
+    }
 
-	public String getLocation() {
-		return location;
-	}
+    public int getPrice() {
+        return price;
+    }
 
-	public void setLocation(String location) {
-		this.location = location;
-	}
+    public void setPrice(int price) {
+        this.price = price;
+    }
 
-	@Override
-	public String toString() {
-		return "Hotel [Id=" + id + ", hotelName=" + hotelName + ", hotelDesc=" + hotelDesc + ", hotelImg1=" + hotelImg1
-				+ ", price=" + price + ", location=" + location + "]";
-	}
+    public String getLocation() {
+        return location;
+    }
 
-	
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Booking> getBooking() {
+        return booking;
+    }
+
+    public void setBooking(List<Booking> booking) {
+        this.booking = booking;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel [id=" + id +
+                ", hotelName=" + hotelName +
+                ", hotelDesc=" + hotelDesc +
+                ", price=" + price +
+                ", location=" + location + "]";
+    }
 }
+
